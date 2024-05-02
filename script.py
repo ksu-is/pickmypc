6e5bcc4ed328d0195650b6ba448bad5ecd6f862e
ngineksu <157438124+ngineksu@users.noreply.github.com>
2024-05-01 20:59:59 -0400

    fixed most solutions, laptop/desktop selection was previously broken.

--- script.py
+++ script.py
@@ -59,25 +59,30 @@ isbudget=budget()
 islord=lord()
 isgaming=gaming()
 iscad=cad()
-print("budget is ",isbudget," laptop or desktop is ",islord," gaming is ",isgaming," cad is ",iscad)
 
-if isbudget == 1 and isgaming == 1 and islord == 2:
-    print("you are in a difficult spot. \n your best best is looking for a local deal where someone's getting rid of an older system. you should look for a system with no older than a 2nd generation i5, 8GB of memory, a small solid state drive, and a video card with no less than 1GB of VRAM. Ask the seller if their system meets these requirements. Good luck.")
 if isbudget == 1 and isgaming == 1 and islord == 1:
+    print("you are in a difficult spot. \n your best best is looking for a local deal where someone's getting rid of an older system. you should look for a system with no older than a 2nd generation i5, 8GB of memory, a small solid state drive, and a video card with no less than 1GB of VRAM. Ask the seller if their system meets these requirements. Good luck.")
+if isbudget == 1 and isgaming == 1 and islord == 2:
     print("your only chance of finding a suitable system is a good deal on an older Dell Precision, HP ZBook, or Lenovo W series system using 2nd-5th generation Intel processors (years 2012-2015). Anything newer will likely exceed your budget.")
 if isbudget == 1 and isgaming == 2 and islord == 1 and iscad == 1:
     print("you may be able to find a local deal for an older system. look for something with a 2nd-6th generation i5, at least 8GB of memory, and a solid state hard drive. Bonus points if you can find a system that has a video card.")
 if isbudget == 1 and isgaming == 2 and islord == 2 and iscad == 1:
     print("you may be able to find a local deal for an older system. look for something with a 2nd-6th generation i5, at least 8GB of memory, and a solid state hard drive.")
-if isbudget == 2 and isgaming == 1 and islord == 2:
-    print("for under $200, you should be able to find an older system with a 2nd-6th generation i5-i7, at least 8GB of memory, a solid state hard drive, and a video card with at least 2GB of VRAM. First, try looking for a system with a 4GB video card.")
+if isbudget == 1 and isgaming == 2 and islord == 1 and iscad == 2:
+    print("look locally or on ebay for a system with a 2nd-4th generation i5/17, at least 8GB of memory, and a solid state hard drive with at least 120GB of storage. You may be able to find one with extra storage, or an additional HDD.")
+if isbudget == 1 and isgaming == 2 and islord == 2 and iscad == 2:
+    print("look locally/on ebay for a laptop with a 2nd-5th generation i5/i7, at least 8GB of memory, and a solid state hard drive with at least 120GB of storage.")
+
+
 if isbudget == 2 and isgaming == 1 and islord == 1:
-    print("for under $200, try to find a 6th generation Intel Dell Precision, HP ZBook, or Lenovo P50 series laptop.")
-if isbudget == 2 and isgaming == 2 and islord == 1 and iscad == 1:
+    print("for under $200, you should be able to find an older system with a 2nd-6th generation i5-i7, at least 8GB of memory, a solid state hard drive, and a video card with at least 2GB of VRAM. First, try looking for a system with a 4GB video card.")
+if isbudget == 2 and isgaming == 1 and islord == 2:
     print("for under $200, try to find a 6th generation Intel Dell Precision, HP ZBook, or Lenovo P50 series laptop.")
 if isbudget == 2 and isgaming == 2 and islord == 2 and iscad == 1:
+    print("for under $200, try to find a 6th generation Intel Dell Precision, HP ZBook, or Lenovo P50 series laptop.")
+if isbudget == 2 and isgaming == 2 and islord == 1 and iscad == 1:
     print("for under $200, try to find a system with a 6th-7th generation Intel processor, at least 8GB of memory, a solid state drive, and a video card with at least 1GB of VRAM.")
-if isbudget == 2 and isgaming == 2 and islord == 1 and iscad == 2:
-    print("for under $200, you should be able to find a laptop with a 6th-8th generation Intel processor, at least 8GB of memory, and a solid state hard drive. This will be a good user experience.")
 if isbudget == 2 and isgaming == 2 and islord == 2 and iscad == 2:
+    print("for under $200, you should be able to find a laptop with a 6th-8th generation Intel processor, at least 8GB of memory, and a solid state hard drive. This will be a good user experience.")
+if isbudget == 2 and isgaming == 2 and islord == 1 and iscad == 2:
     print("for under $200, you should be able to find a desktop with a 6th-8th generation Intel processor, at least 8GB of memory, and a solid state hard drive. This will be a good user experience.")
